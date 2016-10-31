d269461cc01994cd085cad34a53fdddab0237e83
sophiakc <sophiakc@gmail.com>
2016-10-31 13:01:03 -0700

    restart project from scratch

---
+++ Mailbox/MailboxViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MailboxViewController.swift
+//  Mailbox
+//
+//  Created by Sophia KC on 31/10/2016.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
